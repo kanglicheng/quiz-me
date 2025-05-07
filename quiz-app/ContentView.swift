ab8a96ccb30a742c013074712d2456e7c22dc999
kanglicheng <kangli.cheng@gmail.com>
2025-05-07 10:47:01 -0600

    Initial Commit

---
+++ quiz-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quiz-app
+//
+//  Created by Stephen Cheng on 5/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quiz-app/quiz_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiz_appApp.swift
+//  quiz-app
+//
+//  Created by Stephen Cheng on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct quiz_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
